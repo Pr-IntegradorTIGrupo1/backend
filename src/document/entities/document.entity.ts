b3d9d9d6c2e2cdf2163f35d86a592875b9e51399
Carlos Tapia <68973311+c4rloncho@users.noreply.github.com>
2024-05-24 20:33:38 -0400

    relacion de document con forum

--- src/document/entities/document.entity.ts
+++ src/document/entities/document.entity.ts
@@ -5,12 +5,18 @@ import {
   Unique,
   ManyToOne,
   JoinColumn,
+  OneToOne,
 } from 'typeorm';
 import { ObjectType, Field, Int } from '@nestjs/graphql';
+import { Forum } from 'src/forum/entities/forum.entity';
 
 @ObjectType()
 export class Document {
   @PrimaryGeneratedColumn()
   @Field((type) => Int)
   id: number;
+
+  @OneToOne(()=> Forum, forum => forum.document)
+  @Field((type)=>Forum)
+  forum: Forum;
 }
